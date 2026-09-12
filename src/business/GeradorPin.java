07692a4707a54359682602f099a96126eeff2e00
Maria Eduarda Aguiar <ma.aguiareduarda@gmail.com>
2026-09-12 13:45:15 -0300

    Implementa geradores de senha forte e PIN

--- src/business/GeradorPin.java
+++ src/business/GeradorPin.java
@@ -1,10 +1,54 @@
 package business;
 
-public class GeradorPin extends GeradorSenha{
+public class GeradorPin extends GeradorSenha {
 
     @Override
     public String gerarSenha() {
-        // TODO implementar
-        return null;
+
+        String pin;
+
+        do {
+            pin = "";
+
+            for (int i = 0; i < 6; i++) {
+                pin += sortearCaractere(NUMEROS);
+            }
+
+        } while (todosIguais(pin) || sequencial(pin));
+
+        return pin;
+    }
+
+    private boolean todosIguais(String pin) {
+
+        for (int i = 1; i < pin.length(); i++) {
+            if (pin.charAt(i) != pin.charAt(0)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean sequencial(String pin) {
+
+        boolean crescente = true;
+        boolean decrescente = true;
+
+        for (int i = 1; i < pin.length(); i++) {
+
+            int anterior = pin.charAt(i - 1);
+            int atual = pin.charAt(i);
+
+            if (atual != anterior + 1) {
+                crescente = false;
+            }
+
+            if (atual != anterior - 1) {
+                decrescente = false;
+            }
+        }
+
+        return crescente || decrescente;
     }
 }
--- src/business/GeradorSenhaForte.java
+++ src/business/GeradorSenhaForte.java
@@ -5,14 +5,38 @@ public class GeradorSenhaForte extends GeradorSenha {
     private int tamanho;
 
     public GeradorSenhaForte(int tamanho) {
-        // TODO validar tamanho mínimo
+
+        if (tamanho < 12) {
+            throw new IllegalArgumentException(
+                    "A senha forte deve possuir no mínimo 12 caracteres."
+            );
+        }
+
         this.tamanho = tamanho;
     }
 
     @Override
     public String gerarSenha() {
-        // TODO implementar
-        return null;
+
+        String senha = "";
+
+        // Garante pelo menos um caractere de cada tipo
+        senha += sortearCaractere(MAIUSCULAS);
+        senha += sortearCaractere(MINUSCULAS);
+        senha += sortearCaractere(NUMEROS);
+        senha += sortearCaractere(ESPECIAIS);
+
+        // Conjunto com todos os caracteres possíveis
+        String todosCaracteres =
+                MAIUSCULAS + MINUSCULAS + NUMEROS + ESPECIAIS;
+
+        // Completa a senha até atingir o tamanho configurado
+        while (senha.length() < tamanho) {
+            senha += sortearCaractere(todosCaracteres);
+        }
+
+        // Embaralha para evitar que os primeiros caracteres
+        // sempre apareçam na mesma ordem
+        return embaralhar(senha);
     }
-    
 }
